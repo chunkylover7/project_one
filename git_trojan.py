133a02b1de01b12a5e2c81320a6cae06c117ebe3
chunkylover7 <chuckscott@rocketmail.com>
2015-02-02 01:14:44 -0500

    fixed book bug missing path to config dir

--- git_trojan.py
+++ git_trojan.py
@@ -12,7 +12,7 @@ from github3 import login
 
 trojan_id = "abc"
 
-trojan_config = "%s.json" % trojan_id
+trojan_config = "config/%s.json" % trojan_id
 data_path     = "data/%s/" % trojan_id
 trojan_modules= []
 
